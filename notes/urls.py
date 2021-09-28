d7f012cbefb3415db752a3a1db1eabee1bb45f69
ivanabb2 <62958684+ivanabb2@users.noreply.github.com>
2021-09-27 22:30:08 -0300

    continua projeto mais uma vez

--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete', views.deleteNote, name='noteDelete'),
+    path('edit/<int:id>', views.updateNote, name='noteEdit'),
     path('tagsList/', views.tagsList, name='tagsList'),
     path('tag/<str:myTag>/', views.tag, name='tag'),
-    path('api/notes/<int:noteId>', views.api_note),
-    path('api/notes/', views.api_notes),
-    path('api-auth/', include('rest_framework.urls'))
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Note
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import Http404
-from .serializers import NoteSerializer
 
 def deleteNote(meuid):
     note = Note.objects.get(id=meuid)
@@ -15,7 +10,6 @@ def updateNote(meuid, title, content, tag):
     note_item.title, note_item.content = title, content
     note_item.tag = tag
     note_item.save()
-
 
 def createNote(title, content, tag):
     note_item = Note(title=title, content=content, tag=tag)
@@ -56,28 +50,3 @@ def tag(request, myTag):
     notesTag = Note.objects.filter(tag=myTag)
     return render(request, 'notes/tag.html', {'notes': notesTag})
 
-@api_view(['GET', 'POST'])
-def api_note(request, noteId):
-    try:
-        note = Note.objects.get(id=noteId)
-    except Note.DoesNotExist:
-        raise Http404()
-    if request.method == 'POST':
-        new_note_data = request.data
-        note.title = new_note_data['title']
-        note.content = new_note_data['content']
-        note.tag = new_note_data['tag']
-        note.save()
-
-    return Response(NoteSerializer(note).data)
-
-@api_view(['GET', 'POST'])
-def api_notes(request):
-    if request.method == 'POST':
-        newNote = request.data
-        newNote = Note(title=newNote['title'], content=newNote['content'], tag=newNote['tag'])
-        newNote.save()
-
-    all_notes = Note.objects.all()
-
-    return Response(NoteSerializer(all_notes, many=True).data)
